0237bba2f320cb432f0435b6f19a546350bc0112
ArhangelAgrail <skeleton_19@mail.ru>
2021-03-11 21:03:23 +0300

    Update nadeko_installer_latest.sh

--- nadeko_installer_latest.sh
+++ nadeko_installer_latest.sh
@@ -32,13 +32,13 @@ cd "$tempdir"
 
 echo ""
 echo "Downloading NadekoBot, please wait."
-git clone -b master --recursive --depth 1 https://github.com/ArhangelAgrail/LakeBot
+git clone -b master --recursive --depth 1 https://github.com/ArhangelAgrail/NadekoBot
 echo ""
 echo "NadekoBot downloaded."
 
 echo ""
 echo "Downloading Nadeko dependencies"
-cd "$tempdir/NadekoBot"
+cd "$root/$tempdir/NadekoBot"
 dotnet restore
 echo ""
 echo "Download done"
@@ -49,8 +49,7 @@ dotnet build --configuration Release
 echo ""
 echo "Building done. Moving Nadeko"
 
-cd ".."
-cd ".."
+cd "$root"
 
 if [ ! -d NadekoBot ]
 then
@@ -59,19 +58,19 @@ else
     rm -rf NadekoBot_old 1>/dev/null 2>&1
     mv -fT NadekoBot NadekoBot_old 1>/dev/null 2>&1
     mv "$tempdir"/NadekoBot NadekoBot
-    cp -f "NadekoBot_old/src/NadekoBot/credentials.json" "NadekoBot/src/NadekoBot/credentials.json" 1>/dev/null 2>&1
+    cp -f "$root/NadekoBot_old/src/NadekoBot/credentials.json" "$root/NadekoBot/src/NadekoBot/credentials.json" 1>/dev/null 2>&1
     echo ""
     echo "credentials.json copied to the new version"
-    cp -RT "NadekoBot_old/src/NadekoBot/bin/" "NadekoBot/src/NadekoBot/bin/" 1>/dev/null 2>&1
-    cp -RT "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
-	cp -RT "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
-    cp -RT "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
-    mv -f "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
-	mv -f "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot_old.db" 1>/dev/null 2>&1
-    mv -f "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot.db" "NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
+    cp -RT "$root/NadekoBot_old/src/NadekoBot/bin/" "$root/NadekoBot/src/NadekoBot/bin/" 1>/dev/null 2>&1
+    cp -RT "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
+	cp -RT "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
+    cp -RT "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.1/data/NadekoBot.db" 1>/dev/null 2>&1
+    mv -f "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
+	mv -f "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot_old.db" 1>/dev/null 2>&1
+    mv -f "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
     echo ""
     echo "Database copied to the new version"
-    cp -RT "NadekoBot_old/src/NadekoBot/data/" "NadekoBot/src/NadekoBot/data/" 1>/dev/null 2>&1
+    cp -RT "$root/NadekoBot_old/src/NadekoBot/data/" "$root/NadekoBot/src/NadekoBot/data/" 1>/dev/null 2>&1
     echo ""
     echo "Other data copied to the new version"
 fi
@@ -80,6 +79,6 @@ rm -r "$tempdir"
 echo ""
 echo "Installation Complete."
 
-cd ".."
-rm "nadeko_installer_latest.sh"
+cd "$root"
+rm "$root/nadeko_installer_latest.sh"
 exit 0

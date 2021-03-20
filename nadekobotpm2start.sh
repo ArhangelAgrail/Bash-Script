ccde4ef3066cd64a1f5ea00acefa97467ec8be8f
ArhangelAgrail <skeleton_19@mail.ru>
2021-03-21 01:42:24 +0300

    Update nadekobotpm2start.sh

--- nadekobotpm2start.sh
+++ nadekobotpm2start.sh
@@ -17,7 +17,7 @@ while [ $choice -eq 5 ]; do
 read choice
 if [ $choice -eq 1 ] ; then
 	echo ""
-	wget -N https://ArhangelAgrail:12QW34er&ASD@github.com/ArhangelAgrail/Lake-BashScript/raw/master/NadekoARN.sh 
+	wget -N https://ArhangelAgrail:Rp8cfEbAimf9oNOu@github.com/ArhangelAgrail/Lake-BashScript/raw/master/NadekoARN.sh 
 	cd "$root"
 	echo "Starting Nadeko in pm2 with auto-restart and no auto-update..."
 	sudo pm2 start "$root/NadekoARN.sh" --interpreter=bash --name=Lake
@@ -28,7 +28,7 @@ if [ $choice -eq 1 ] ; then
 else
 	if [ $choice -eq 2 ] ; then
 		echo ""
-		wget -N https://ArhangelAgrail:12QW34er&ASD@github.com/ArhangelAgrail/Lake-BashScript/raw/master/NadekoARU_Latest.sh 
+		wget -N https://ArhangelAgrail:Rp8cfEbAimf9oNOu@github.com/ArhangelAgrail/Lake-BashScript/raw/master/NadekoARU_Latest.sh 
 		cd "$root"
 		echo "Starting Nadeko in pm2 with auto-restart and auto-update..."
 		sudo pm2 start "$root/NadekoARU_Latest.sh" --interpreter=bash --name=Lake
@@ -39,7 +39,7 @@ else
 	else
 		if [ $choice -eq 3 ] ; then
 		echo ""
-		wget -N https://ArhangelAgrail:12QW34er&ASD@github.com/ArhangelAgrail/Lake-BashScript/raw/master/nadeko_run.sh
+		wget -N https://ArhangelAgrail:Rp8cfEbAimf9oNOu@github.com/ArhangelAgrail/Lake-BashScript/raw/master/nadeko_run.sh
 		cd "$root"
 		echo "Starting Nadeko in pm2 normally without any auto update or restart.."
 		sudo pm2 start "$root/nadeko_run.sh" --interpreter=bash --name=Lake

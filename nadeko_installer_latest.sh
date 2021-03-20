c891c539d840298116c333dc785b5cd576c500f4
ArhangelAgrail <skeleton_19@mail.ru>
2021-03-21 02:06:49 +0300

    Update nadeko_installer_latest.sh

--- nadeko_installer_latest.sh
+++ nadeko_installer_latest.sh
@@ -32,7 +32,7 @@ cd "$tempdir"
 
 echo ""
 echo "Downloading NadekoBot, please wait."
-git clone -b master --recursive --depth 1 https://ArhangelAgrail:Rp8cfEbAimf9oNOu@github.com/ArhangelAgrail/LakeBot
+git clone -b master --recursive --depth 1 https://github.com/ArhangelAgrail/LakeBot
 echo ""
 echo "NadekoBot downloaded."
 
